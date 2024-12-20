6bad22b6ee390f067ea0d78cdb7d5ee705e6ee59
Aideveloper-dev <mariamazzouz95@gmail.com>
2024-12-20 19:25:55 +0100

    Add files via upload

---
+++ app2/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_cors import CORS
+from app.config import init_app, db, login_manager
+from app.models.user import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+def create_app():
+    app = init_app()
+    
+    # Configure CORS properly
+    CORS(app, resources={
+        r"/*": {
+            "origins": "*",
+            "methods": ["GET", "POST", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
+
+    # Register blueprints
+    from app.routes.auth import auth
+    from app.routes.avatar import avatar
+    #from app.routes.clothing import clothing
+    from app.routes.clothing import clothing_bp
+
+    app.register_blueprint(auth)
+    app.register_blueprint(avatar, url_prefix='/avatar')
+    #app.register_blueprint(clothing, url_prefix='/clothing')
+
+    
+    app.register_blueprint(clothing_bp)
+    # Create database tables
+    with app.app_context():
+        db.create_all()
+
+    return app 
